4821d31df65a1564331e3c9cd8822e46fcf6c463
WASHINGTON TEMPONE DA SILVA <washington_t_silva@estudante.sesisenai.org.br>
2025-04-22 22:05:33 -0300

    salvar exercicios alex

---
+++ html/sobreVoce.js
@@ -0,0 +1,16 @@
+function exercicio1(){
+    let n1 = Number(prompt("Digite um numero"))
+    let n2 = Number(prompt("Digite um numero"))
+    let soma= n1+n2
+    alert("A soma de  dois numero é: " +soma )
+    alert(`A soma de  dois numero é: ${soma }`)
+
+}
+function exercicio2(){
+    let n1 = Number(prompt("Digite um numero"))
+    let n2 = Number(prompt("Digite um numero"))
+    let media= (n1+n2) / 2
+    alert("A média de  dois numero é: " +media )
+   
+
+}
