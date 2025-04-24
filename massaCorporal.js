b032c8db767949cb78fd5ab6abcbc854f39c6198
WASHINGTON TEMPONE DA SILVA <washington_t_silva@estudante.sesisenai.org.br>
2025-04-24 18:56:10 -0300

    senai

--- massaCorporal.js
+++ massaCorporal.js
@@ -9,3 +9,24 @@
 // Entre 25 e 29,9: Sobrepeso
 // 30 ou mais: Obesidade
 
+function massaCorporal(){
+    alert ("Vamos medir sua massa corporal.")
+    let peso = Number( prompt("Qual é seu peso? "))
+    let altura= Number(prompt("Qual é sua altura?"))
+    let resultado= peso  /(altura*altura)
+
+    if(resultado <=18.5){
+       alert("Abaixo do peso.")
+    }
+     else if(resultado >18.5  <24.9){
+       alert("Peso normal.")
+    }
+    else  if(resultado >25 <29.9){
+      alert("Acima do peso.")
+    }  
+    else if(resultado>30){ 
+        alert("Obesidade.")
+    }
+    
+
+}
