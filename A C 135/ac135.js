054351834f229fabd682342b5c6880226dd242f8
washingtontempone <wsilva920@yahoo.com>
2025-05-26 17:51:51 -0300

    alex aula

---
+++ A C 135/ac135.js
@@ -0,0 +1,8 @@
+function cadastro(){
+document.getElementById('cadastro').style.display= 'flex'
+document.getElementById('cadastroObreiro').style.display='none'
+}
+function cadastroObreiro(){
+   document.getElementById('cadastro').style.display= "flex"
+document.getElementById('cadastroObreiro').style.display= 'none'
+}
